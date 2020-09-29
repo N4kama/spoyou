d6d82cca21b0789b29b5587a7242eb569fdf4209
Romain Baguet <romain.baguet@epita.fr>
2020-09-29 22:07:50 +0200

    [SPOYOU] arguments to command line added, possible to give playlist url directly and output file

--- spoyou.py
+++ spoyou.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import sys
+import getopt
 import json
 # URL conversions.
 import urllib.request
@@ -8,6 +10,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 # Youtube library
 import youtube
+
 
 def get_tracks_from_spotify_playlist(config, playlist_url):
     # Authenticating to Spotify
@@ -43,24 +46,51 @@ def get_tracks_from_spotify_playlist(config, playlist_url):
 
     return trackList
 
+
 def search_track_on_youtube(config, track):
     api = youtube.API(client_id=config["youtube"]["client_id"],
-              client_secret=config["youtube"]["client_secret"],
-              api_key=config["youtube"]["api_key"])
-    video = api.get('search', q=track, maxResults=1, type='video', order='relevance')
+                      client_secret=config["youtube"]["client_secret"],
+                      api_key=config["youtube"]["api_key"])
+    video = api.get('search', q=track, maxResults=1,
+                    type='video', order='relevance')
     return("https://www.youtube.com/watch?v="+video["items"][0]["id"]["videoId"])
 
+
+"""
+    Main function
+"""
 if (__name__ == "__main__"):
+    playlist_url = None
+    outputfile = None
+
+    # Checking arguments
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:], "hp:o:", ["help", "playlistUrl=", "output="])
+    except getopt.GetoptError:
+        print(
+            "./spoyou.py [-h | --help] [-p playlist_url | --playlistUrl playlist_url] [-o file | --output file]")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h' or opt == '--help':
+            print(
+                "./spoyou.py [-h | --help] [-p playlist_url | --playlistUrl playlist_url] [-o file | --output file]")
+            sys.exit()
+        elif opt in ("-p", "--playlistUrl"):
+            playlist_url = arg
+        elif opt in ("-o", "--output"):
+            outputfile = arg
+
     # Get config
     print("Reading config.json...")
     with open("config.json") as json_file:
         config = json.load(json_file)
 
     # Get all tracks from public playlist
-    spotify_tracks = get_tracks_from_spotify_playlist(
-        config,
-        str(input("Insert Spotify playlist URL: ")))
-    print("Tracks retrieved from spotify!")
+    if not playlist_url:
+        playlist_url = str(input("Insert Spotify playlist URL: "))
+    spotify_tracks = get_tracks_from_spotify_playlist(config, playlist_url)
+    print("{} tracks retrieved from spotify!".format(len(spotify_tracks)))
 
     # Search youtube track
     songs = []
@@ -71,4 +101,9 @@ if (__name__ == "__main__"):
     print("URL LIST: ")
     for i in songs:
         print(i)
-    
+
+    if outputfile:
+        print("Writting URLs to file : {}".format(outputfile))
+        with open(outputfile) as url_file:
+            for s in songs:
+                url_file.write(s)
